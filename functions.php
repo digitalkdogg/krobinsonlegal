71606fa23ab6ce3cb4752383a76f5678ce097e89
Kevin Bollman <digitalkdogg@gmail.com>
2025-12-02 11:31:58 -0600

    added legal js into the site

--- functions.php
+++ functions.php
@@ -5,12 +5,16 @@
  * (Please see https://developer.wordpress.org/themes/advanced-topics/child-themes/#how-to-create-a-child-theme)
  */
 add_action( 'wp_enqueue_scripts', 'vantage_child_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'my_custom_scripts' );
 function vantage_child_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
         array('parent-style')
     );
+}
+function my_custom_scripts() {
+    wp_enqueue_script( 'my-script', get_stylesheet_directory_uri() . '/legal.js', array('jquery'), '1.0', true );
 }
 /*
  * Your code goes below
