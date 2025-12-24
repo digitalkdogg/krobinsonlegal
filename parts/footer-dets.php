1121dfe8e8bb4253d170f8602ce47057fd1ce421
Kevin Bollman <digitalkdogg@gmail.com>
2025-12-24 10:52:34 -0600

    update to the footer dets

--- parts/footer-dets.php
+++ parts/footer-dets.php
@@ -8,9 +8,9 @@
     Mailing Address :  P.O. Box 1027, Harrison, AR 72602 <br />
     <br />
     
-    <div class = "flex justify-around">
-        <a href = '/contact'><button>Contact Us</button></a>
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/img/courthouse-404.png" style = "height: 100px; width: 200px; opacity: 0.5;" aria-hidden="true" />
+    <div class = "flex justify-around margin-bottom-20">
+        <a href = '/contact'><button class="margin-bottom-20">Contact Us</button></a>
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/img/krl_logo_brown.webp" style = "filter: brightness(2); height: 100px; width: 150px; opacity: 0.5;" aria-hidden="true" />
     </div>
     <span class = "copyright-text">Copyright &copy; <?php echo date("Y"); ?> <?php bloginfo('name'); ?> All Rights Reserved<span>
 
